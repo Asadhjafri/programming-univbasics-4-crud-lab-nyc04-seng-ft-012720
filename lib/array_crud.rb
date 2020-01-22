78882fbbf50474847104249330152aba07c736b4
Asad Jafri <asadhjafri@gmail.com>
2020-01-22 18:40:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,12 +30,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array = create_an_array = ["wow", "whey", "am", "pre workout", "aminos", "casein",]
+  create_an_array = ["wow", "whey", "am", "pre workout", "aminos", "casein",]
   create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  create_an_array = ["wow", "whey", "am", "pre workout", "aminos", "casein", "arrays!"]
+  create_an_array[6]
 end
 
 def update_element_from_index(array, index_number, element)
